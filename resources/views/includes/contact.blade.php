a61b491f1c1d6a499bc934afc3215b1c7a4ef241
Benjamin Orinoloye <bjfikky@yahoo.com>
2017-05-27 18:38:22 -0400

    setting up home page using blade templating engine done till home blog

---
+++ resources/views/includes/contact.blade.php
@@ -0,0 +1,46 @@
+<!-- CONTACT -->
+<section class="contact" id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="center-title">
+                <h1>GET IN TOUCH</h1>
+                <p>We diminution preference thoroughly if. Joy deal pain view much her time. Led young gay would now state. Pronounce we attention admitting on assurance of suspicion conveying</p>
+            </div>
+            <div class="col-md-12">
+                <div class="row">
+                    <!-- contact-informations -->
+                    <div class="col-md-offset-2 col-md-8 contact-information top_45">
+                        <div class="col-md-4 contact-info text-center"> <i class="fa fa-phone" aria-hidden="true"></i>
+                            <p>49 0216 514 25 25
+                                <br>0216 514 25 25</p>
+                        </div>
+                        <div class="col-md-4 contact-info text-center"> <i class="fa fa-map-marker" aria-hidden="true"></i>
+                            <p>107 Santa Monica Boulevard Los Angeles, California</p>
+                        </div>
+                        <div class="col-md-4 contact-info text-center"> <i class="fa fa-envelope" aria-hidden="true"></i>
+                            <p>info@alberto.com
+                                <br/> contact@alberto.com</p>
+                        </div>
+                    </div>
+                    <!-- form -->
+                    <form class="col-md-offset-2 col-md-8 contact-form top_60" method="POST" action="mail.php">
+                        <div class="row">
+                            <!-- name -->
+                            <div class="col-md-6">
+                                <input id="con_name" class="form-input requie" type="text" placeholder="Name">
+                            </div>
+                            <!-- email -->
+                            <div class="col-md-6">
+                                <input id="con_email" class="form-input requie" type="text" placeholder="Email">
+                            </div>
+                            <!--message-->
+                            <div class="col-md-12">
+                                <textarea id="con_message" class="form-text requie" placeholder="Message" rows="8"></textarea>
+                            </div>
+                            <input id="con_submit" class="giggs_button2" type="submit" value="SEND A MESSAGE"> </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
